8799180fb9ed0bea8640a916412ec7f0392c0ea4
Fernando <fernando_mcunha@hotmail.com>
2025-02-10 20:51:49 -0300

    Add seed script | Add gems

--- Gemfile
+++ Gemfile
@@ -21,6 +21,8 @@ gem "bootsnap", require: false
 # Add HTTP asset caching/compression and X-Sendfile acceleration to Puma [https://github.com/basecamp/thruster/]
 gem "thruster", require: false
 
+gem 'httparty'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,42 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+require 'httparty'
+
+USER_COUNT = 100
+POST_COUNT = 200_000
+IP_ADDRESSES = Array.new(50) { Faker::Internet.unique.ip_v4_address }
+RATING_PROBABILITY = 0.75
+
+def create_post(login, ip)
+  response = HTTParty.post(
+    'http://localhost:3000/api/posts', 
+    headers: { 'Content-Type' => 'application/json' },
+    body: { title: Faker::Lorem.sentence, body: Faker::Lorem.paragraph, login: login, ip: ip }.to_json
+  )
+
+  JSON.parse(response.body)['post']['id']
+end
+
+def create_rating(post_id, user_id, value)
+  HTTParty.post(
+    'http://localhost:3000/api/ratings', 
+    headers: { 'Content-Type' => 'application/json' },
+    body: { post_id: post_id, user_id: user_id, value: value }.to_json
+  )
+end
+
+users = Array.new(USER_COUNT) { Faker::Lorem.unique.characters(number: 6) }
+
+POST_COUNT.times do |i|
+  login = users.sample
+  ip = IP_ADDRESSES.sample
+  post_id = create_post(login, ip)
+
+  if rand < RATING_PROBABILITY
+    rating_user_id = User.find_by(login: login).id
+    create_rating(post_id, rating_user_id, rand(1..5))
+  end
+
+  puts "Created #{i + 1} posts" if (i + 1) % 5_000 == 0
+end
+
+puts 'Done!'
